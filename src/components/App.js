43368f08b84e10ee82abdad1bf6b3beba45e3a02
raulvictorrosa <raulvictorrosa@gmail.com>
2019-02-26 13:41:34 -0300

    feat: initial app

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Chips from "./Chips";
+
+const App = () => {
+  return (
+    <div className="base-page">
+      <div className="container ">
+        <div className="row ">
+          <div className="col">
+            <Chips />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Chip from "./components/Chip.js";
+import App from "./components/App";
+import "./styles/main.scss";
 
-import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      <Chip avatar={"https://goo.gl/imoVYg"} />
-
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, document.getElementById("root"));
