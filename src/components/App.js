ac45442cfac20c432c3e6df8606704fdb6aa0269
raulvictorrosa <raulvictorrosa@gmail.com>
2019-02-28 20:32:40 -0300

    feat: Balloon component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Balloon from "./Balloon";
 import Card from "./Card";
 import Cards from "./Cards";
 import Chips from "./Chips";
@@ -13,6 +14,22 @@ const App = () => {
           </div>
           <div className="col" style={{ marginTop: "30px" }}>
             <Cards />
+          </div>
+          <div className="col" style={{ marginTop: "30px" }}>
+            <h3>Balloon</h3>
+            <Balloon
+              referral={
+                <Card
+                  title="Vivamus suscipit"
+                  image={require(`../images/image6.jpg`)}
+                >
+                  Vivamus suscipit tortor eget felis porttitor volutpat. Quisque
+                  velit nisi, pretium ut lacinia in, elementum id enim.
+                </Card>
+              }
+            >
+              Balloon text message
+            </Balloon>
           </div>
         </div>
       </div>
---
+++ src/components/Balloon.js
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const root = document.getElementById("root");
+
+const FunBalloon = props => {
+  const {
+    isHidden,
+    text,
+    positions: { x, y }
+  } = props;
+
+  return (
+    <div
+      className="balloon"
+      style={{
+        display: isHidden ? "none" : "block",
+        left: x,
+        top: y
+      }}
+    >
+      {text}
+    </div>
+  );
+};
+
+class Balloon extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isHidden: true,
+      mouse: {
+        x: 0,
+        y: 0
+      }
+    };
+
+    this.el = document.createElement("div");
+    this.toogleVisibility = this.toogleVisibility.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+  }
+
+  componentDidMount() {
+    root.appendChild(this.el);
+  }
+
+  componentDidUpdate() {
+    this.renderFunBalloon();
+  }
+
+  renderFunBalloon() {
+    const { isHidden, mouse } = this.state;
+    const { children } = this.props;
+
+    ReactDOM.render(
+      <FunBalloon isHidden={isHidden} positions={mouse} text={children} />,
+      this.el
+    );
+  }
+
+  toogleVisibility() {
+    this.setState({
+      isHidden: !this.state.isHidden
+    });
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      mouse: {
+        x: event.pageX + 5,
+        y: event.pageY + 5
+      }
+    });
+  }
+
+  render() {
+    const { mouse } = this.state;
+    const { referral } = this.props;
+
+    return (
+      <span style={{ display: "inline-block" }}>
+        <span
+          style={{ display: "inline-block" }}
+          onMouseEnter={this.toogleVisibility}
+          onMouseLeave={this.toogleVisibility}
+          onMouseMove={this.handleMouseMove}
+        >
+          {referral}
+          <p>
+            The current mouse position is ({mouse.x}, {mouse.y})
+          </p>
+        </span>
+      </span>
+    );
+  }
+}
+
+export default Balloon;
