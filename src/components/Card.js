a5c7a72a9353d0178e4f6f06351749642fd76512
raulvictorrosa <raulvictorrosa@gmail.com>
2019-02-28 20:30:00 -0300

    feat: Single card component

---
+++ src/components/Card.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+function Card(props) {
+  const { className, children, title, teste } = props;
+  let { image } = props;
+
+  const cardId = title ? title.replace(/\W/g, "-").toLowerCase() : "";
+
+  return (
+    <div id={cardId} className={`card ${className || ""}`}>
+      <div className="card-content custom-opacity">
+        <div
+          className="card-media"
+          style={{
+            backgroundImage: image
+              ? `url("${image}")`
+              : `url("${require("../images/no-image-found.png")}")`
+          }}
+        />
+        <div className="card-body">
+          <h2>{title}</h2>
+          <p>{children}</p>
+        </div>
+      </div>
+      <div className="card-actions">
+        <a className="custom-opacity" href={`#${cardId}`}>
+          Share
+        </a>
+        <a className="custom-opacity" href={`#${cardId}`}>
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
